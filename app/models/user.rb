650a659355c743a1b57f89cc162580b96a583423
Matheus Silva <matheus.souza28042001@gmail.com>
2024-04-14 21:24:50 -0300

    refactor: update default password validation of Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}\z/
+  validates :password, format: { with: PASSWORD_REGEX, message: "must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long" }, if: :password_required?
+
+  private
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
 end
