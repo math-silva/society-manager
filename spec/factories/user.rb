7ecedeacd826692611fc11ef1ac8fb3f69adb3e1
Matheus Silva <matheus.souza28042001@gmail.com>
2024-04-17 20:07:53 -0300

    refactor: update User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    first_name { 'John Doe' }
+    first_name { 'Test' }
     last_name { 'User' }
     email { 'test@example.org' }
     password { 'Password1!' }
