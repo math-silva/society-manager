074dc5721073cbbb59b162986f8aa2180a23c1b1
Matheus Silva <matheus.souza28042001@gmail.com>
2024-04-17 20:08:33 -0300

    test: update User validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,94 +2,78 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "validations" do
-    let(:valid_user) { create(:user) }
+    let(:user) { create(:user) }
 
     it "is valid with valid attributes" do
-      expect(valid_user).to be_valid
+      expect(user).to be_valid
+    end
+
+    it "is invalid without a first name" do
+      user.first_name = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "is invalid without a last name" do
+      user.last_name = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:last_name]).to include("can't be blank")
     end
 
     it "is invalid without an email" do
-      invalid_user = valid_user.dup
-      invalid_user.email = nil
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:email]).to include("can't be blank")
-    end
-
-    it "is invalid without a password" do
-      invalid_user = valid_user.dup
-      invalid_user.password = nil
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:password]).to include("can't be blank")
+      user.email = nil
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("can't be blank")
     end
 
     it "is invalid with a duplicate email" do
-      valid_user.save!
-      duplicate_user = valid_user.dup
+      user.save!
+      duplicate_user = user.dup
       expect(duplicate_user).not_to be_valid
       expect(duplicate_user.errors[:email]).to include("has already been taken")
     end
 
     it "is invalid with a duplicate email (case insensitive)" do
-      valid_user.save!
-      duplicate_user = valid_user.dup
-      duplicate_user.email = valid_user.email.upcase
+      user.save!
+      duplicate_user = user.dup
+      duplicate_user.email = user.email.upcase
       expect(duplicate_user).not_to be_valid
       expect(duplicate_user.errors[:email]).to include("has already been taken")
     end
 
     it "is invalid with a password without at least one letter" do
-      invalid_user = valid_user.dup
-      invalid_user.password = "12345678"
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
+      user.password = "12345678"
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
     end
 
     it "is invalid with a password without at least one digit" do
-      invalid_user = valid_user.dup
-      invalid_user.password = "Password"
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
+      user.password = "Password"
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
     end
 
     it "is invalid with a password without at least one special character" do
-      invalid_user = valid_user.dup
-      invalid_user.password = "Password1"
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
+      user.password = "Password1"
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
     end
 
     it "is invalid with a password without a uppercase letter" do
-      invalid_user = valid_user.dup
-      invalid_user.password = "password1!"
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
+      user.password = "password1!"
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
     end
 
     it "is invalid with a password shorter than 8 characters" do
-      invalid_user = valid_user.dup
-      invalid_user.password = "Pass1!"
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
-    end
-
-    it "is invalid without a first name" do
-      invalid_user = valid_user.dup
-      invalid_user.first_name = nil
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:first_name]).to include("can't be blank")
-    end
-
-    it "is invalid without a last name" do
-      invalid_user = valid_user.dup
-      invalid_user.last_name = nil
-      expect(invalid_user).not_to be_valid
-      expect(invalid_user.errors[:last_name]).to include("can't be blank")
+      user.password = "Pass1!"
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long")
     end
 
     it "is valid with a password containing multiple special characters" do
-      valid_user_multiple_sp_char = valid_user.dup
-      valid_user_multiple_sp_char.password = "Password1!@#$%"
-      expect(valid_user_multiple_sp_char).to be_valid
+      user.password = "Password1!@#$%"
+      expect(user).to be_valid
     end
   end
 end
