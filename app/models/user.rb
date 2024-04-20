33189c2d226b984b814177d0ce3018ddc5ba5bed
Matheus Silva <matheus.souza28042001@gmail.com>
2024-04-19 22:34:09 -0300

    refactor: update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,10 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
 
   PASSWORD_REGEX = /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}\z/
-  validates :password, format: { with: PASSWORD_REGEX, message: "must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long" }, if: :password_required?
-
-  private
-
-  def password_required?
-    !persisted? || !password.nil? || !password_confirmation.nil?
-  end
+  validates :password, 
+            format: { 
+              with: PASSWORD_REGEX, 
+              message: "must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long" 
+            },
+            presence: true
 end
