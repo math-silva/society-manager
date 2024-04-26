31bfe44b9273547697ce1ebe6fdf195faff6c534
Matheus Silva <matheus.souza28042001@gmail.com>
2024-04-26 16:57:15 -0300

    feat: add tournament model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_223633) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_121943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tournaments", force: :cascade do |t|
+    t.string "name"
+    t.string "sport"
+    t.string "tournament_type"
+    t.integer "teams_count"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
